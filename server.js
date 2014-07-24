0caae65e6f9da792dcc8fbea63de3dd4eb2ff048
Taneli Hartikainen <taneli.hartikainen@jamk.fi>
2014-07-24 13:11:53 +0300

    Remove handlers for ticket-events.
    Remove ticket-event handlers, because the new teamboard-client does
    not use any socket.emit functionality apart from board:join and :leave.
    Remove methods other than GET from socket.request.

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ var io     = require('socket.io')();
 var redis  = require('socket.io-redis')
 var config = require('./config');
 
-// setup redis storage adapter for scalability
+// use redis as central memory store for socket.io servers
 io.adapter(redis({ host: config.redis.host, port: config.redis.port }));
 
-// handshaking via authentication api
-// requires connecting user to have a valid access-token
+// clients must provide a valid access-token
 io.use(function(socket, next) {
-
 	var token = socket.request._query['access-token'];
 
 	if(!token) {
@@ -20,12 +18,8 @@ io.use(function(socket, next) {
 
 	var request = require('./utils/request')(token);
 
-	socket.request.get    = request.get;
-	socket.request.put    = request.put;
-	socket.request.post   = request.post;
-	socket.request.delete = request.delete;
-
-	request.get('auth', function(err, user) {
+	socket.request.get = request.get;
+	socket.request.get('auth', function(err, user) {
 
 		if(err) {
 			return next(err);
@@ -40,13 +34,8 @@ io.use(function(socket, next) {
 var controllers = require('./controllers');
 
 io.sockets.on('connection', function(socket) {
-
 	socket.on('board:join',  controllers.board.join(socket));
 	socket.on('board:leave', controllers.board.leave(socket));
-
-	socket.on('ticket:create', controllers.ticket.create(socket));
-	socket.on('ticket:update', controllers.ticket.update(socket));
-	socket.on('ticket:remove', controllers.ticket.remove(socket));
 });
 
 module.exports = io;
--- test/board.js
+++ test/board.js
@@ -35,78 +35,77 @@ describe('controllers/board', function() {
 		clientOutsideRoom.removeAllListeners('board:leave');
 	});
 
-	describe('board:join', function() {
+	var _test = function(action) {
+		return function() {
 
-		it('should require the board id', function(done) {
-			this.client.emit('board:join', { asd: 'asd' }, function(err, data) {
+			it('should require the board id', function(done) {
+				this.client.emit('board:' + action + '', { asd: 'asd' },
+					function(err, data) {
 
-				err.should.be.ok;
-				err.should.be.a.String;
+						err.should.be.ok;
+						err.should.be.a.String;
 
-				return done();
+						return done();
+					});
 			});
-		});
 
-		it('should respond with board and user data', function(done) {
-			this.client.emit('board:join', { board: 'YYZ123' },
-				function(err, data) {
+			it('should respond with board and user data', function(done) {
+				this.client.emit('board:' + action + '', { board: 'YYZ123' },
+					function(err, data) {
 
-					if(err) {
-						return done(err);
-					}
+						if(err) {
+							return done(err);
+						}
+
+						data.should.be.an.Object;
+						data.should.have.properties(['user', 'board']);
+
+						// we return only identifiers
+						data.user.should.be.a.String;
+						data.board.should.be.a.String;
+
+						return done();
+					});
+			});
+
+			it('should broadcast to the board', function(done) {
+
+				clientInsideRoom.on('board:' + action + '', function(data) {
 
 					data.should.be.an.Object;
 					data.should.have.properties(['user', 'board']);
-
-					// we return only identifiers
-					data.user.should.be.a.String;
-					data.board.should.be.a.String;
+					data.board.should.equal('YYZ123');
 
 					return done();
 				});
-		});
 
-		it('should broadcast to the board', function(done) {
-
-			clientInsideRoom.on('board:join', function(data) {
-
-				data.should.be.an.Object;
-				data.should.have.properties(['user', 'board']);
-				data.board.should.equal('YYZ123');
-
-				return done();
+				var testClient = require('socket.io-client')(this.server, {
+					multiplex: false, query: 'access-token=123ASD' });
+				testClient.on('connect', function() {
+					testClient.emit('board:' + action + '', { board: 'YYZ123' });
+				});
 			});
 
-			var testClient = require('socket.io-client')(this.server, {
-				multiplex: false, query: 'access-token=123ASD' });
-			testClient.on('connect', function() {
-				testClient.emit('board:join', { board: 'YYZ123' });
+			it('should not broadcast globally', function(done) {
+
+				clientOutsideRoom.on('board:' + action + '', function(data) {
+					return done(new Error(
+						'' + action + ' was broadcasted globally'));
+				});
+
+				clientInsideRoom.on('board:' + action + '', function(data) {
+					return done();
+				});
+
+				var testClient = require('socket.io-client')(this.server, {
+					multiplex: false, query: 'access-token=123ASD' });
+				testClient.on('connect', function() {
+					testClient.emit('board:' + action + '', { board: 'YYZ123' });
+				});
 			});
-		});
+		}
+	}
 
-		it('should not broadcast globally', function(done) {
-
-			clientOutsideRoom.on('board:join', function(data) {
-				return done(new Error('Join was broadcasted globally'));
-			});
-
-			clientInsideRoom.on('board:join', function(data) {
-				return done();
-			});
-
-			var testClient = require('socket.io-client')(this.server, {
-				multiplex: false, query: 'access-token=123ASD' });
-			testClient.on('connect', function() {
-				testClient.emit('board:join', { board: 'YYZ123' });
-			});
-		});
-	});
-
-	describe('board:leave', function() {
-
-		it.skip('should require the board id');
-		it.skip('should respond with board and user data');
-		it.skip('should broadcast to the board');
-		it.skip('should not broadcast globally');
-	});
+	describe('board:join',  _test('join'));
+	describe('board:leave', _test('leave'));
 });
