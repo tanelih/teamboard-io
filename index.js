ff9ec2bf7f29e272ec3ad0f4aba7a03ce03056ba
Taneli Hartikainen <taneli.hartikainen@jamk.fi>
2014-10-15 12:20:42 +0300

    Make the server able to start...

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 'use strict';
 
-// var io     = require('./server');
+var server = require('./server');
 var config = require('./config');
 
-var request = require('request');
-
-request.get(config.api + '/auth', function(err, res, body) {
-	console.log(err, res, body);
-});
-
-// io.listen(config.port);
+server.listen(config.port);
 console.log('socket.io listening at port: ', config.port);
